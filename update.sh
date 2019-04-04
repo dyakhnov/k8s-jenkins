a35ef2b00b60604ea5349bb92c9405ac66d2a375
Dmitry Yakhnov <dmitry@yakhnov.info>
2019-04-04 11:06:13 +1100

    Change repo to official

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-docker build . -t dyakhnov/jenkins:latest
-docker push dyakhnov/jenkins:latest
+# Build
+docker build . -t mytardis/k8s-jenkins:latest
+
+# Push
+docker push mytardis/k8s-jenkins:latest
+
+# Update
+kubectl -n jenkins scale statefulset.apps/jenkins --replicas=0
+sleep 3
+kubectl -n jenkins scale statefulset.apps/jenkins --replicas=1
